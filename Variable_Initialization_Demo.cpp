e70b2916bbafba4d97c8ec17436293697e54d59f
Muhammad Zaka ILLAHi <babrumbabrum12@gmail.com>
2026-03-10 22:44:45 +0500

    Implement multi-type variable initialization logic
    The program demonstrates initializing and updating integer, float, and double types via console input, highlighting the basic structure of C++ data allocation."

---
+++ Variable_Initialization_Demo.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+	
+	int value1;
+	value1=5;
+	
+	float value2=32.5;
+	double value3=70.5;	
+	
+	cout<<"Enter a number"<<endl;
+	cin>>value1;
+	cout<<"(integer result):"<<value1<<endl<<endl; 
+
+	cout<<"Enter a number"<<endl;
+	cin>>value2;
+	cout<<"(float result):"<<value2<<endl<<endl; 
+
+	cout<<"Enter a number"<<endl;
+	cin>>value3;
+	cout<<"(double result):"<<value3<<endl<<endl; 
+
+
+}
